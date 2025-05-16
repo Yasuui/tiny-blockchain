fb159ace7c1e3bbdda4c277533bd5e6f3c260746
Yonis Diriye <yonisdiriye35@gmail.com>
2025-05-15 21:35:27 -0400

    Add simple proof of work consensus algorithm

--- src/blockchain/blockchain.py
+++ src/blockchain/blockchain.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import hashlib
 import json
+from .proof_of_work import ProofOfWork
 
 class Block:
     def __init__(self, index, transactions, timestamp, previous_hash):
@@ -8,6 +9,7 @@ class Block:
         self.timestamp = timestamp
         self.transactions = transactions
         self.previous_hash = previous_hash
+        self.nonce = 0
         self.hash = self.calculate_hash()
 
     def calculate_hash(self):
@@ -15,9 +17,9 @@ class Block:
             "index": self.index,
             "timestamp": self.timestamp,
             "transactions": self.transactions,
-            "previous_hash": self.previous_hash
+            "previous_hash": self.previous_hash,
+            "nonce": self.nonce
         }, sort_keys=True).encode()
-        
         return hashlib.sha256(block_string).hexdigest()
 
     def to_dict(self):
@@ -28,7 +30,8 @@ class Block:
             "timestamp": self.timestamp,
             "transactions": txs,
             "previous_hash": self.previous_hash,
-            "hash": self.hash
+            "hash": self.hash,
+            "nonce": self.nonce
         }
 
 class Blockchain:
@@ -51,7 +54,8 @@ class Blockchain:
             datetime.now().timestamp(),
             self.get_latest_block().hash
         )
-        
+        pow = ProofOfWork(block, self.difficulty)
+        block.hash = pow.mine()
         self.chain.append(block)
         self.pending_transactions = []
 
@@ -64,12 +68,10 @@ class Blockchain:
 
     def get_balance(self, address):
         balance = 0
-        
         for block in self.chain:
             for transaction in block.transactions:
                 if transaction["from"] == address:
                     balance -= transaction["amount"]
                 if transaction["to"] == address:
                     balance += transaction["amount"]
-                    
         return balance 
--- src/blockchain/proof_of_work.py
+++ src/blockchain/proof_of_work.py
@@ -2,8 +2,29 @@ import hashlib
 import json
 
 class ProofOfWork:
-    def __init__(self, difficulty=4):
+    def __init__(self, block, difficulty=4):
+        self.block = block
         self.difficulty = difficulty
+        self.nonce = 0
+
+    def mine(self):
+        target = '0' * self.difficulty
+        while True:
+            self.block.nonce = self.nonce
+            block_hash = self.calculate_hash()
+            if block_hash.startswith(target):
+                return block_hash
+            self.nonce += 1
+
+    def calculate_hash(self):
+        block_string = json.dumps({
+            "index": self.block.index,
+            "timestamp": self.block.timestamp,
+            "transactions": self.block.transactions,
+            "previous_hash": self.block.previous_hash,
+            "nonce": self.block.nonce
+        }, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
 
     def get_proof_of_work(self, block):
         block_string = json.dumps({
